1e8b3eb5dd211737264e25a9dfd141c64438997f
erkin312 <erkin.kk@gmail.com>
2021-05-31 22:28:21 +0600

    fetching

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,32 +1,65 @@
+import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://www.nomadasaurus.com/wp-content/uploads/2019/06/Photos-of-Kyrgyzstan.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://www.state.gov/wp-content/uploads/2019/04/Kyrgyzstan-2198x1406.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
+// const DUMMY_DATA = [
+//   {
+//     id: 'm1',
+//     title: 'This is a first meetup',
+//     image:
+//       'https://www.nomadasaurus.com/wp-content/uploads/2019/06/Photos-of-Kyrgyzstan.jpg',
+//     address: 'Meetupstreet 5, 12345 Meetup City',
+//     description:
+//       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//   },
+//   {
+//     id: 'm2',
+//     title: 'This is a second meetup',
+//     image:
+//       'https://www.state.gov/wp-content/uploads/2019/04/Kyrgyzstan-2198x1406.jpg',
+//     address: 'Meetupstreet 5, 12345 Meetup City',
+//     description:
+//       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+//   },
+// ];
 
 const AllMeetupsPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      'https://react-meetup-app-5b7e2-default-rtdb.firebaseio.com/meetups.json'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   return (
     <section>
       <h1>All Meetups</h1>
       <ul>
-        <MeetupList meetups={DUMMY_DATA} />
+        <MeetupList meetups={loadedMeetups} />
       </ul>
     </section>
   );
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,29 @@
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
+  const history = useHistory();
+
+  const addMeetupHandler = (meetupData) => {
+    axios(
+      'https://react-meetup-app-5b7e2-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        data: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  };
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
