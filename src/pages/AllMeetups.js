745f82dd637a0954bd609c350ae5191395a4fb0e
erkin312 <erkin.kk@gmail.com>
2021-05-31 14:54:32 +0600

    klgadklgh

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,35 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_DATA = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image:
+      'https://www.nomadasaurus.com/wp-content/uploads/2019/06/Photos-of-Kyrgyzstan.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://www.state.gov/wp-content/uploads/2019/04/Kyrgyzstan-2198x1406.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+];
+
 const AllMeetupsPage = () => {
-  return <div>All Meetups Page</div>;
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <ul>
+        <MeetupList meetups={DUMMY_DATA} />
+      </ul>
+    </section>
+  );
 };
 
 export default AllMeetupsPage;
