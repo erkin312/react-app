821694058b3700b92cdc9cb3388d0f0c71200c50
erkin312 <erkin.kk@gmail.com>
2021-05-31 13:47:42 +0600

    added routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div>Hello!</div>;
-}
+import { Route, Switch } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupPage from './pages/NewMeetup';
+
+const App = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <AllMeetupsPage />{' '}
+        </Route>
+        <Route path="new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
